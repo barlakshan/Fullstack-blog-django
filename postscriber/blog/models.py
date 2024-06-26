e2e62adfe3319c023fe01861f1f492a2b91cba8b
barlakshan <ravindu.ba.lakshan@gmail.com>
2024-06-26 22:48:23 +0530

    blog 1.1.0

--- postscriber/blog/models.py
+++ postscriber/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PostModel(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Title')
+    content = models.TextField(verbose_name='Content')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Author')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Date Created')
+    
+    class Meta:
+        ordering = ('-date_created',)
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+    
+    def __str__(self) -> str:
+        return self.title
